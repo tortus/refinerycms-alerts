5babd8032046275c2b60309787e683ec702299eb
William Makley <wmakley@tortus.com>
2014-12-12 16:49:29 -0500

    remove front-end routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Refinery::Core::Engine.routes.draw do
 
-  # Frontend routes
-  namespace :alerts do
-    resources :alerts, :path => '', :only => [:index, :show]
-  end
-
   # Admin routes
   namespace :alerts, :path => '' do
     namespace :admin, :path => Refinery::Core.backend_route do
