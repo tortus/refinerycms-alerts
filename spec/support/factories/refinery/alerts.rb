1fe3558aa22ff045599a188d98de37a532da514f
William Makley <wmakley@tortus.com>
2015-01-23 13:17:47 -0500

    add alert factory traits for live and expired

--- spec/support/factories/refinery/alerts.rb
+++ spec/support/factories/refinery/alerts.rb
@@ -2,5 +2,13 @@ FactoryGirl.define do
   factory :alert, :class => Refinery::Alerts::Alert do
     sequence(:title) { |n| "RefineryAlert-#{n}" }
     live_at { Time.zone.now }
+
+    trait :live do
+      live_at { Time.zone.now }
+    end
+
+    trait :expired do
+      down_at { Time.zone.now }
+    end
   end
 end
