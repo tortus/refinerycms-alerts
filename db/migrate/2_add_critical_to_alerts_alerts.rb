0a5f5a8f3ad4588392ee8fa8b300318bfe984dc9
William Makley <wmakley@tortus.com>
2016-10-13 15:48:03 -0400

    fix migration class name

--- db/migrate/2_add_critical_to_alerts_alerts.rb
+++ db/migrate/2_add_critical_to_alerts_alerts.rb
@@ -1,4 +1,4 @@
-class CreateAlertsAlerts < ActiveRecord::Migration
+class AddCriticalToAlertsAlets < ActiveRecord::Migration
 
   def change
     add_column :refinery_alerts, :critical, :boolean, :null => false, :default => false
